0218051a0219d10f7aafc31a8fd33a7b910e7a98
haeori <seungyeon.dev@gmail.com>
2025-03-06 11:17:21 +0900

    refactor: 공백 라인 추가하여 가독성 향상
    - 코드 가독성 향상을 위해 의미 구간 단락별 공백 라인 추가

--- src/main/java/cleancode/minesweeper/tobe/MinesweeperGame.java
+++ src/main/java/cleancode/minesweeper/tobe/MinesweeperGame.java
@@ -8,6 +8,7 @@ public class MinesweeperGame {
 
     public static final int BOARD_ROW_SIZE = 8;
     public static final int BOARD_COL_SIZE = 10;
+    public static final Scanner SCANNER = new Scanner(System.in);
     private static final String[][] BOARD = new String[BOARD_ROW_SIZE][BOARD_COL_SIZE];
     private static final Integer[][] NEARBY_LAND_MINE_COUNTS = new Integer[BOARD_ROW_SIZE][BOARD_COL_SIZE];
     private static final boolean[][] LAND_MINES = new boolean[BOARD_ROW_SIZE][BOARD_COL_SIZE];
@@ -16,7 +17,6 @@ public class MinesweeperGame {
     public static final String LAND_MINE_SIGN = "☼";
     public static final String CLOSED_CELL_SIGN = "□";
     public static final String OPENED_CELL_SIGN = "■";
-    static Scanner SCANNER = new Scanner(System.in); // Scanner 객체가 계속 생성되지 않도록 상수로 선언
 
     // 상수와 필드 구분 개행
     private static int gameStatus = 0; // 0: 게임 중, 1: 승리, -1: 패배
@@ -24,8 +24,12 @@ public class MinesweeperGame {
     public static void main(String[] args) {
         showGameStartComments();
         initializeGame();
+        // 이니셜라이즈 종료되므로 의미 단락 분리
+
         while (true) {
             showBoard();
+            // showBoard 후 게임 진행시키므로 의미 단락 분리
+
             if (doesUserWinTheGame()) { // 추상화 레벨이 맞지 않는 구체적 로직을 메서드로 추출
                 System.out.println("지뢰를 모두 찾았습니다. GAME CLEAR!");
                 break;
@@ -34,6 +38,7 @@ public class MinesweeperGame {
                 System.out.println("지뢰를 밟았습니다. GAME OVER!");
                 break;
             }
+
             String cellInput = getCellInputFromUser(); // 사용할 변수가 가깝게 선언되도록 변경하였다가, 객체가 매번 재생성되어 상수로 변경
             String userActionInput = getUserActionInputFromUser();
             actOnCell(cellInput, userActionInput); // 특정 좌표에서 수행할 행동을 분리한 메서드
@@ -43,11 +48,14 @@ public class MinesweeperGame {
     private static void actOnCell(String cellInput, String userActionInput) {
         int selectedColIndex = getSelectedColIndex(cellInput);
         int selectedRowIndex = getSelectedRowIndex(cellInput);
+
         if (doesUserChooseToPlantFlag(userActionInput)) { // if-else 무분별한 사용 대신 if로 조건 기술 및 early return 적용
             BOARD[selectedRowIndex][selectedColIndex] = FLAG_SIGN;
             checkIfGameIsOver();
             return;
         }
+        // if문 끝날 때마다 환기를 위해 의미 단락 분리
+
         if (doesUserChooseToOpenCell(userActionInput)) {
             if (isLandMineCell(selectedRowIndex, selectedColIndex)) {
                 BOARD[selectedRowIndex][selectedColIndex] = LAND_MINE_SIGN;
@@ -124,16 +132,6 @@ public class MinesweeperGame {
                 .noneMatch(cell -> cell.equals(CLOSED_CELL_SIGN));
     }
 
-// 이해를 위해 임시로 주석처리하여 남겨둠
-//    private static boolean isAllCellOpenedWithAnnotation() {
-//        Stream<String[]> stringArrayStream = Arrays.stream(BOARD); // 이중 배열인 BOARD String 배열에 stream을 걸어 생성(이해를 위해 변수로 추출)
-//        Stream<String> stringStream = stringArrayStream.flatMap(stringArray -> { // string array를 다시 가져와서 stream을 건다
-//            Stream<String> stringStream2 = Arrays.stream(stringArray); // flatMap이 아니라 Map이면 Stream<Stream<String>>이 되므로 flatMap으로 평탄화함
-//            return stringStream2;
-//        }); // 즉 stringStream은 BOARD의 각 원소들(string)을 다 평탄화해서 stream으로 만든 것
-//        return stringStream.noneMatch(cell -> cell.equals(CLOSED_CELL_SIGN)); // cell이 BOARD의 각 원소들이며, 모든 셀이 CLOSED_CELL_SIGN이 아니면 true 반환
-//    }
-
     private static int convertRowFrom(char cellInputRow) {
         int selectedRowIndex = Character.getNumericValue(cellInputRow) - 1;
         return selectedRowIndex;
@@ -184,11 +182,14 @@ public class MinesweeperGame {
                 BOARD[row][col] = CLOSED_CELL_SIGN;
             }
         }
+        // 반복문 종료시마다 작업이 하나 끝난 것이므로 환기를 위해 단락 분리
+
         for (int i = 0; i < LAND_MINE_COUNT; i++) {
             int col = new Random().nextInt(BOARD_COL_SIZE);
             int row = new Random().nextInt(BOARD_ROW_SIZE);
             LAND_MINES[row][col] = true; // 값을 할당하는 부분이므로 isLandMineCell(row, col)로 변경하지 않음
         }
+
         for (int row = 0; row < BOARD_ROW_SIZE; row++) {
             for (int col = 0; col < BOARD_COL_SIZE; col++) {
                 int count = 0;
